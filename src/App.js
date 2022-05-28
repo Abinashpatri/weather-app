e521f3d82cf70efe3f947e459e06cf46403d23e4
Abinash Patri <abinashpatri33@gmail.com>
2022-05-28 09:13:42 +0530

    v.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [cond, setCond] = useState([]);
+
+  const url = `http://api.weatherstack.com/current?access_key=d3166618b751ff3708e84044f2b10320&query=${query}`;
+
+  async function getData() {
+    const result = await axios.get(url);
+    setCond([result.data]);
+    console.log(result.data);
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitForm} className="form">
+        <input
+          type="text"
+          placeholder="enter the place"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Enter</button>
+      </form>
+      {cond.map((item, index) => {
+        return (
+          <div key={index} className="items">
+            <div className="place">
+              <img src={item.current.weather_icons} alt="" />
+              <p>{item.location.country}</p>
+              <p>{item.location.region}</p>
+
+              <div className="condition">
+                <p>{item.current.observation_time}</p>
+                <p>{item.current.temperature}</p>
+                <p>
+                  <span>Pressure</span> : {item.current.pressure}
+                </p>
+                <p>
+                  <span>Wind degree</span> : {item.current.wind_degree}
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
